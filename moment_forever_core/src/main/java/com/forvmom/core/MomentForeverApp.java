ccfad113a544947a80d61fc0e1eb3ff2735825dc
unknown <pri24064@adobe.com>
2026-03-02 14:35:25 +0530

    migration to api gateway

--- moment_forever_core/src/main/java/com/forvmom/core/MomentForeverApp.java
+++ moment_forever_core/src/main/java/com/forvmom/core/MomentForeverApp.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {
         "com.forvmom"
 })
 @EntityScan(basePackages = {
         "com.forvmom.data.entities",
-        "com.forvmom.security.entities"  // If security has entities
+        "com.forvmom.security.entities" // If security has entities
 })
 
 public class MomentForeverApp {
